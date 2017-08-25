24d63b15682ec5898c1771fc42eade288af0bb5b
Fluffer-Nutter <jimmy.zarbock@gmail.com>
2017-08-25 14:16:53 -0500

    First Upload {8-25-2017}
    Login screen "Layout" finished. (This will be very basic since I wont use it but others might want to.)

---
+++ Script-Kiddies-Trainer/Script-Kiddies-Trainer/GUI/LogIn.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Script_Kiddies_Trainer
+{
+
+    public partial class LogIn : Window
+    {
+        public LogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left) this.DragMove();
+        }
+
+        private void LogIn_Close_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
